063630079db96ac5e90bfacfbfc4502b8e46953f
Pereradilon9 <86822439+Pereradilon9@users.noreply.github.com>
2023-09-03 22:20:44 +0530

    Update function.js

--- function.js
+++ function.js
@@ -1,24 +1,12 @@
-// function.js
-
-function calculateHmacSha1(message, secretKey) {
-    const cryptoBuffer = new TextEncoder().encode(secretKey);
-    const dataBuffer = new TextEncoder().encode(message);
-
-    return crypto.subtle.importKey(
-        "raw",
-        cryptoBuffer,
-        { name: "HMAC", hash: { name: "SHA-1" } },
-        false,
-        ["sign"]
-    ).then((key) => {
-        return crypto.subtle.sign(
-            "HMAC",
-            key,
-            dataBuffer
-        );
-    }).then((signature) => {
-        const hashArray = Array.from(new Uint8Array(signature));
-        const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
-        return hashHex;
-    });
+window.function = function (input, key) {
+  const message = input.value;
+  const secretKey = key.value;
+  try {
+    const hmacSha1 = CryptoJS.HmacSHA1(message, key);
+    const hexEncodedHash = hmacSha1.toString(CryptoJS.enc.Hex);
+    return hexEncodedHash;
+  }
+  catch(error) {
+    return "Error loading CryptoJS:";
+  }
 }
