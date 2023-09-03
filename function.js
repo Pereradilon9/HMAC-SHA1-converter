b9d4e888de6f9bcb6d4fe30825a25406b14f1a0d
Pereradilon9 <86822439+Pereradilon9@users.noreply.github.com>
2023-09-03 21:32:01 +0530

    Update function.js

--- function.js
+++ function.js
@@ -1,5 +1,24 @@
-const message = 'hello';
-const secretKey = 'secret';
-const hmac = CryptoJS.HmacSHA1(message, secretKey);
-const hashHex = hmac.toString(CryptoJS.enc.Hex);
-console.log(hashHex)
+// function.js
+
+function calculateHmacSha1(message, secretKey) {
+    const cryptoBuffer = new TextEncoder().encode(secretKey);
+    const dataBuffer = new TextEncoder().encode(message);
+
+    return crypto.subtle.importKey(
+        "raw",
+        cryptoBuffer,
+        { name: "HMAC", hash: { name: "SHA-1" } },
+        false,
+        ["sign"]
+    ).then((key) => {
+        return crypto.subtle.sign(
+            "HMAC",
+            key,
+            dataBuffer
+        );
+    }).then((signature) => {
+        const hashArray = Array.from(new Uint8Array(signature));
+        const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+        return hashHex;
+    });
+}
