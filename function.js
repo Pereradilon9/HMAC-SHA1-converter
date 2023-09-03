1ceeed954f0cb7cf4c7e86d731aa9c5ff7e70e4e
Pereradilon9 <86822439+Pereradilon9@users.noreply.github.com>
2023-09-03 21:03:25 +0530

    again to console.log()

--- function.js
+++ function.js
@@ -2,4 +2,4 @@ const message = 'hello';
 const secretKey = 'secret';
 const hmac = CryptoJS.HmacSHA1(message, secretKey);
 const hashHex = hmac.toString(CryptoJS.enc.Hex);
-return hashHex
+console.log(hashHex)
