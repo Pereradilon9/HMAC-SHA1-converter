1d2119e2553884334af62d3669d50c952740f437
Pereradilon9 <86822439+Pereradilon9@users.noreply.github.com>
2023-09-03 22:37:10 +0530

    Update function.js

--- function.js
+++ function.js
@@ -2,11 +2,11 @@ window.function = function (input, key) {
   const message = input.value;
   const secretKey = key.value;
   try {
-    const hmacSha1 = CryptoJS.HmacSHA1(message, key);
+    const hmacSha1 = CryptoJS.HmacSHA1(message, secretKey); // Use secretKey here
     const hexEncodedHash = hmacSha1.toString(CryptoJS.enc.Hex);
     return hexEncodedHash;
   }
-  catch(error) {
-    return "Error loading CryptoJS:";
+  catch (error) {
+    return "Error loading CryptoJS: " + error; // Include the error message
   }
 }
