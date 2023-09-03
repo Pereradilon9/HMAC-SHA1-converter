cb6ed8f2cfa7473a9da3dd4446ba27407328222d
Pereradilon9 <86822439+Pereradilon9@users.noreply.github.com>
2023-09-04 00:49:14 +0530

    Update driver.js

--- driver.js
+++ driver.js
@@ -1,28 +1,35 @@
 // Add an event listener to wait for the CryptoJS script to load
 document.addEventListener('DOMContentLoaded', function () {
-  // Attach your existing message event listener here
-  window.addEventListener("message", async function (event) {
-    const { origin, data: { key, params } } = event;
-    let result;
-    let error;
-    try {
-      result = await window.function(...params);
-    } catch (e) {
-      result = undefined;
+  if (typeof CryptoJS === 'undefined') {
+    //console.error('CryptoJS not loaded');
+  } else {
+    // Attach your existing message event listener here
+    window.addEventListener("message", async function (event) {
+      const { origin, data: { key, params } } = event;
+
+      let result;
+      let error;
       try {
-        error = e.toString();
+        result = await window.function(...params);
       } catch (e) {
-        error = "Exception can't be stringified.";
+        result = undefined;
+        try {
+          error = e.toString();
+        } catch (e) {
+          error = "Exception can't be stringified.";
+        }
       }
-    }
-    const response = { key };
-    if (result !== undefined) {
-      // FIXME: Remove `type` once that's in staging
-      response.result = { value: result };
-    }
-    if (error !== undefined) {
-      response.error = error;
-    }
-    event.source.postMessage(response, "*");
-  });
+
+      const response = { key };
+      if (result !== undefined) {
+        // FIXME: Remove `type` once that's in staging
+        response.result = { value: result };
+      }
+      if (error !== undefined) {
+        response.error = error;
+      }
+
+      event.source.postMessage(response, "*");
+    });
+  }
 });
